c2f094fb487458d1e4755764b62cf93264ceea37
chanchal26 <chanchal.mch2613@gmail.com>
2022-11-27 20:48:51 +0600

    admin route done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,6 +13,26 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.v73oziy.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+// function verifyJWT(req, res, next) {
+//     const authHeader = req.headers.authorization;
+//     console.log(req.headers);
+//     if (!authHeader) {
+//         return res.status(401).send('unauthorized access');
+//     }
+
+//     const token = authHeader.split(' ')[1];
+
+//     jwt.verify(token, process.env.WEB_TOKEN, function (err, decoded) {
+//         if (err) {
+//             return res.status(403).send({ message: 'forbidden access' })
+//         }
+//         req.decoded = decoded;
+//         next();
+//     })
+// }
+
 
 async function dbConnect() {
     try {
@@ -26,6 +47,31 @@ async function dbConnect() {
             const user = req.body;
             const result = await Users.insertOne(user);
             res.send(result)
+        });
+
+        app.get('/users/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email }
+            const user = await Users.findOne(query);
+            res.send({ isAdmin: user?.adminRole === 'admin' });
+        });
+
+        app.get('/users/seller/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email }
+            const user = await Users.findOne(query);
+            res.send({ isSeller: user?.role === 'Seller' });
+        });
+
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const user = await Users.findOne(query);
+            if (user) {
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' });
+                return res.send({ accessToken: token })
+            }
+            res.status(403).send({ accessToken: '' })
         });
 
 
@@ -45,13 +91,18 @@ async function dbConnect() {
             const email = req.params.email;
             const result = await AllPhones.find({ email: email }).toArray();
             res.send(result);
-        })
+        });
 
 
         app.post('/advertise', async (req, res) => {
             const advertise = req.body;
             const result = await AdvertiseItem.insertOne(advertise);
             res.send(result)
+        });
+
+        app.get('/advertise', async (req, res) => {
+            const result = await AdvertiseItem.find().toArray();
+            res.send(result);
         });
 
 
@@ -93,16 +144,19 @@ async function dbConnect() {
             const result = await AllPhones.find(query).toArray();
             res.send(result)
         });
+
         app.get('/lg', async (req, res) => {
             const query = { brand: "LG" };
             const result = await AllPhones.find(query).toArray();
             res.send(result)
         });
+
         app.get('/mi', async (req, res) => {
             const query = { brand: "MI" };
             const result = await AllPhones.find(query).toArray();
             res.send(result)
         });
+
         app.get('/onePlus', async (req, res) => {
             const query = { brand: "OnePlus" };
             const result = await AllPhones.find(query).toArray();
